a2c2aa52c7ee7e24250f043f2a51361e808b4e2f
DronTS <109414199+DronTC@users.noreply.github.com>
2023-11-19 17:52:32 +0300

    add AlertDialog

--- app/src/main/java/com/example/firstproject/add_user_activity.kt
+++ app/src/main/java/com/example/firstproject/add_user_activity.kt
@@ -1,10 +1,14 @@
 package com.example.firstproject
 
+import android.app.AlertDialog
 import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -14,6 +18,7 @@ class add_user_activity : AppCompatActivity() {
         setContentView(R.layout.activity_add_user)
 
         val viewDate: TextView = findViewById(R.id.view_date)
+        val buttonShowAlertDialog: Button = findViewById(R.id.button_show_alert_dialog)
         val buttonDatePicker : Button = findViewById(R.id.select_date_button)
 
         GettingCurrentDate(viewDate)
@@ -31,6 +36,11 @@ class add_user_activity : AppCompatActivity() {
             DatePickerDialog(this, datePicker, myCalendar.get(Calendar.YEAR),
                 myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show()
         }
+
+        buttonShowAlertDialog.setOnClickListener {
+            ShowDialog()
+        }
+
     }
     fun GettingCurrentDate(viewDate : TextView){
         val dateFormat = SimpleDateFormat("d MMM yyyy")
@@ -42,5 +52,19 @@ class add_user_activity : AppCompatActivity() {
     fun UpdateLable(myCalendar: Calendar, viewDate: TextView){
         val sdf = SimpleDateFormat("d MMM yyyy")
         viewDate.text = "Дата ${sdf.format(myCalendar.time)}"
+    }
+    fun ShowDialog(){
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.add_dialog_layout, null)
+        val builder = AlertDialog.Builder(this)
+            .setView(mDialogView)
+            .setTitle("Создание нового события")
+
+        val mAlertDialog = builder.show()
+        val button : Button = mDialogView.findViewById(R.id.dialogBtn)
+        val editText : EditText = mDialogView.findViewById(R.id.et_editText)
+        button.setOnClickListener {
+            mAlertDialog.dismiss()
+            Toast.makeText(this, editText.text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
